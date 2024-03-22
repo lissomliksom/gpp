17dd50c3a3549112547176c4b7bc8ae292ebf5a3
Simen Kristoffersen <lissomliksom@gmail.com>
2024-03-22 22:06:38 +0100

    Add TailwindCSS, remove old styles

--- src/components/DisplayReps.vue
+++ src/components/DisplayReps.vue
@@ -8,7 +8,7 @@
 </script>
 
 <template>
-  <div class="container">
-    <p>{{ randomRepetitionVariation }}</p>
+  <div class="flex items-center justify-center p-3 text-xl">
+    {{ randomRepetitionVariation }}
   </div>
 </template>
